b75e8f011fcbbb7fb7fce38106fd1b28a03fa605
RubyHuynh <hthn.oo@gmail.com>
2020-05-03 13:55:43 +0000

    enum meta

--- boost/enum.cpp
+++ boost/enum.cpp
@@ -1,8 +1,9 @@
 #include <boost/regex.hpp>
 #include <boost/xpressive/xpressive.hpp>
 #include <iostream>
+#include <ostream>
 
-using namespace boost::xpressive;
+//using namespace boost::xpressive;
 
 enum FRUIT {
     BANANA = 9,
@@ -13,9 +14,9 @@ enum FRUIT {
 
 template <typename E, E V> const void str() {
     std::string rs = __PRETTY_FUNCTION__;
-    boost::regex pat( "^[^=]*= ([^;]*)[^=]*= ([^\\]]*)]" );
+    boost::regex pat( "[^=]*= ([^;]*)[^=]*= ([^\\]]*)]" );
     boost::smatch matches;
-    if (boost::regex_match(rs, matches, pat) 
+    if (boost::regex_match(rs, matches, pat))
         std::cout << matches[1] << "=" << matches[2] << std::endl;
     // never fails
 }
--- boost/tuple.cpp
+++ boost/tuple.cpp
@@ -3,7 +3,7 @@
 #include <string>
 #include <iostream>
 #include <ctime>
-
+#include <typeinfo>
 using namespace boost::fusion;
 
 struct dump {
@@ -23,6 +23,24 @@ typedef tuple<int, std::string, bool, double> tupleType;
 //     ~Reference() {};
 // };
 
+template<typename A, typename B, typename C>
+struct tutu {
+    A a;
+    B b;
+    C c;
+    A getA() { return a;}
+    B getB() { return b;}
+    C getC() { return c;}
+    
+    template<typename T>
+    void print() {
+        if(typeid(T) == typeid(A)) std::cout << a;
+        else if(typeid(T) == typeid(B)) std::cout << b;
+        else  std::cout << c;
+
+    }
+};
+
 int main() {
     tupleType x(10, "Fusion", true, 3.14);
     // time_t now = time(0);
@@ -31,5 +49,9 @@ int main() {
     // Reference r(99, "Demo meeting", *gmtm);
     for_each(x, dump());
     //for_each(r, dump());
+    std::cout << "\n\n\n\n\n\n";
+    tutu<int, char, double>tu = {9, 'b', 67.8};
+    tu.print<int>();
+    std::cout << std::endl << tu.getC();
     std::cout << std::endl;
 }
